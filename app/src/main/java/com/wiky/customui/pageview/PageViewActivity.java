ee39850c7d059cee1ba76666b275332cd55bc8da
wiiiky <wiiiky@outlook.com>
2015-04-03 10:06:34 +0800

    sample

--- app/src/main/java/com/wiky/customui/pageview/PageViewActivity.java
+++ app/src/main/java/com/wiky/customui/pageview/PageViewActivity.java
@@ -26,7 +26,8 @@ public class PageViewActivity extends ActionBarActivity {
         ArrayList<PageAdapterImpl.PageItem> list = new ArrayList<>();
         list.add(new PageAdapterImpl.PageItem(R.drawable.pikaqiu, "hello world", "#ff0000"));
         list.add(new PageAdapterImpl.PageItem(R.drawable.xidada, "asjdfajsdfjalkadfjadfakdfkas;df", "#00ff00"));
-        list.add(new PageAdapterImpl.PageItem(R.drawable.pikaqiu, "hello Everyone", "#0000ff"));
+        list.add(new PageAdapterImpl.PageItem(R.drawable.nsapassword, "hello Everyone", "#0000ff"));
+        list.add(new PageAdapterImpl.PageItem(R.drawable.b823kajsdfe, "I don't understand why people believe in so much thing that is ridiculous", "#ff00ff"));
         mPageView.setAdapter(new PageAdapterImpl(this, list));
 
         mPageView.setOnPageChangeListener(new PageView.OnPageChangeListener() {
